bd083077f8c1a3a761e9bcc8c296a6c5c57ed007
Crouman1000 <cr0j.gh@outlook.com>
2025-07-09 23:24:44 -0400

    Added opponentAI

--- pong/opponentAI.py
+++ pong/opponentAI.py
@@ -8,7 +8,7 @@ import game_constants as const
 
 def calculatePaddlePosition(p_p2_paddle: paddle.Paddle, p_ball: ball.Ball):
             
-    
+    ### AI V1
     if p_p2_paddle.y > 0: 
         if p_ball.centery < p_p2_paddle.centery:
             p_p2_paddle.move(-1*const.PADDLE_SPEED)
